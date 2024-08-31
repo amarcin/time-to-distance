126f56352ff8444c0ab55915ef2fe922b6e1ded4
Augustin Marcin <102642895+amarcin@users.noreply.github.com>
2024-08-31 05:34:08 +0000

    few updates, not finished yet

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,16 @@ st.title("Convert Time to Distance Driven")
 # first argument takes the titleof the selectionbox
 # second argument takes options
 
-input = st.number_input("", min_value=0, value=2)
+col1, col2 = st.columns(2)
 
-unit = st.selectbox("",
-                     ['seconds', 'minutes', 'hours', 'days'], index=2)
+with col1:
+    input = st.number_input("", min_value=0, value=2)
+
+with col2: 
+    unit = st.selectbox("",
+                     ['minutes', 'hours', 'days'], index=1)
 
 # Equation
-
 # I need to have different classses of time periods. 
 # Ex. If the car has been driving between 0 and 20 seconds, it goes x speed 
 # 20 sec - 1 min = x speed 
@@ -20,14 +23,21 @@ unit = st.selectbox("",
 # (freeway) 10 min - 20 min = x speed
 # 20+ min
 
+# Thinking, exponentially increasing speed, with a cap at 70 mph
+
 # Ideally, these speeds would be scaling, need to draw it on a graph
-ouput = 0
+output = 0
+
+st.write(output)
 
 if(unit=='seconds'):
     output = input * 5
 
+if (unit=='hours'):
+    output = input * 50
+
 
 if(st.button("Convert")):
-    st.write("", output, unit)
+    st.write("", output, " miles")
 
 st.header("Conversion")
